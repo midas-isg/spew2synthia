396ca2988decee9666732fd15c4b60f33241cf75
sahawut <sahawut@gmail.com>
2017-04-17 22:07:03 -0400

    Generated number of people and emptied income in household and added headers for GQs for Canada and fixed according to code inspection for canada.py.

--- canada.py
+++ canada.py
@@ -4,10 +4,10 @@ import aid
 import conf
 import spew
 
-prefix2csvs = {}
-reversed_sex = {'1': '2', '2': '1'}
-max_age = 99
-age_group2age = {
+_prefix2csvs = {}
+_reversed_sex = {'1': '2', '2': '1'}
+_max_age = 88
+_age_group2age = {
     '1': aid.str_cycle(range(0, 5)),
     '2': aid.str_cycle(range(5, 7)),
     '3': aid.str_cycle(range(7, 10)),
@@ -28,60 +28,61 @@ age_group2age = {
     '18': aid.str_cycle(range(70, 75)),
     '19': aid.str_cycle(range(75, 80)),
     '20': aid.str_cycle(range(80, 85)),
-    '21': aid.str_cycle(range(85, max_age)),
-    '88': aid.str_cycle([max_age])
+    '21': aid.str_cycle(range(85, _max_age)),
+    '88': aid.str_cycle([_max_age])
 }
 
 
 def translate(iso3):
     code = iso3
-    pp_csvs = find_csvs_by_iso3_and_prefix(iso3, conf.pp_prefix)
+    pp_csvs = _find_csvs_by_iso3_and_prefix(iso3, conf.pp_prefix)
 
-    hh_ids = translate_pp(pp_csvs, code)
-    cross_check_hids(iso3, hh_ids)
-    translate_hh(pp_csvs, code)
+    hid2cnt = _translate_pp(pp_csvs, code)
+    _cross_check_hids(iso3, hid2cnt.keys() | set())
+    _translate_hh(pp_csvs, hid2cnt, code)
     aid.touch_require_file(code, 'schools')
     aid.touch_require_file(code, 'workplaces')
-    aid.touch_file(aid.output_csv_synth_file_path(code, 'gq').replace('.csv', '.txt'))
-    aid.touch_file(aid.output_csv_synth_file_path(code, 'gq_people').replace('.csv', '.txt'))
 
 
-def translate_pp(pp_csvs, code):
+def _translate_pp(pp_csvs, code):
     pp_csv = aid.output_csv_synth_file_path(code, 'people')
-    hh_ids = out_pp_file(pp_csvs, pp_csv)
-    save_pp_as_txt_with_reordering_columns(pp_csv)
-    return hh_ids
+    gq_pp_csv = aid.output_csv_synth_file_path(code, 'gq_people')
+    hid2cnt = _save_pp_as_csv(pp_csvs, pp_csv, gq_pp_csv)
+    _save_pp_as_txt_with_reordering_columns(pp_csv, gq_pp_csv)
+    return hid2cnt
 
 
-def cross_check_hids(iso3, hh_ids):
-    hh_csvs = find_csvs_by_iso3_and_prefix(iso3, conf.hh_prefix)
-    ref_hh_ids = to_household_ids(hh_csvs)
+def _cross_check_hids(iso3, hh_ids):
+    hh_csvs = _find_csvs_by_iso3_and_prefix(iso3, conf.hh_prefix)
+    ref_hh_ids = _to_household_ids(hh_csvs)
     difference = hh_ids.difference(ref_hh_ids)
     if difference:
         msg = 'Household IDs from people and household inputs are different:'
         raise Exception(msg, difference)
 
 
-def translate_hh(pp_csvs, code):
+def _translate_hh(pp_csvs, hid2cnt, code):
     hh_csv = aid.output_csv_synth_file_path(code, 'households')
-    out_hh_file(pp_csvs, hh_csv)
-    save_hh_as_txt_with_reordering_columns(hh_csv)
+    gq_csv = aid.output_csv_synth_file_path(code, 'gq')
+    _save_hh_as_csv(pp_csvs, hid2cnt, hh_csv, gq_csv)
+    _save_hh_as_txt_with_reordering_columns(hh_csv, gq_csv)
 
 
-def out_pp_file(in_file_paths, pp_path):
+def _save_pp_as_csv(in_file_paths, pp_path, gq_pp_path):
     """  0 SERIALNO,puma_id,place_id,SYNTHETIC_HID,longitude,
          5 latitude,AGEGRP,HRSWRK,IMMSTAT,INCTAX,
         10 MODE,OCC,POB,RELIGION,SEX,
         15 SYNTHETIC_PID """
     hid_column = 3
-    sex_column = 14
     agegrp_column = 6
+    sex_column = 14
     more_headers = 'made-sporder,made-empty,made-sex,made-age'
     columns = 20
     hid2cnt = {}
     aid.mkdir(pp_path)
-    with open(pp_path, 'w') as pp_csv:
-        print('writing', os.path.abspath(pp_path))
+    aid.mkdir(gq_pp_path)
+    with open(pp_path, 'w') as pp_csv, open(gq_pp_path, 'w') as gq_pp_csv:
+        print('writing', os.path.abspath(pp_path), os.path.abspath(gq_pp_path))
         file_count = 0
         for in_file_path in in_file_paths:
             with open(in_file_path, 'r') as fin:
@@ -94,6 +95,7 @@ def out_pp_file(in_file_paths, pp_path):
                             continue
                         row = line + ',' + more_headers
                         aid.write_and_check_columns(pp_csv, row, columns)
+                        aid.write_and_check_columns(gq_pp_csv, row, columns)
                     else:
                         cells = line.split(',')
                         sex = cells[sex_column]
@@ -101,17 +103,18 @@ def out_pp_file(in_file_paths, pp_path):
                         hid = cells[hid_column]
                         order = hid2cnt.get(hid, 0) + 1
                         hid2cnt[hid] = order
-                        row = line + ',' + ','.join([
+                        row = ','.join([
+                            line,
                             str(order),
                             '',
-                            reversed_sex.get(sex, sex),
-                            to_age(agegroup)
+                            _reversed_sex.get(sex, sex),
+                            _to_age(agegroup)
                         ])
                         aid.write_and_check_columns(pp_csv, row, columns)
-    return hid2cnt.keys() | set()
+    return hid2cnt
 
 
-def to_household_ids(in_file_paths):
+def _to_household_ids(in_file_paths):
     """ SERIALNO,puma_id,place_id,SYNTHETIC_HID,longitude, latitude """
     hid_column = 3
     result = set()
@@ -126,17 +129,17 @@ def to_household_ids(in_file_paths):
     return result
 
 
-def out_hh_file(in_file_paths, hh_path):
+def _save_hh_as_csv(in_file_paths, hid2cnt, hh_path, gq_path):
     """  0 SERIALNO,puma_id,place_id,SYNTHETIC_HID,longitude,
          5 latitude,AGEGRP,HRSWRK,IMMSTAT,INCTAX,
         10 MODE,OCC,POB,RELIGION,SEX,
         15 SYNTHETIC_PID """
     hid_column = 3
-    more_header = 'made-empty'
-    columns = 17
+    more_header = 'made-empty,made-persons'
+    columns = 18
     aid.mkdir(hh_path)
-    with open(hh_path, 'w') as hh_csv:
-        print('writing', os.path.abspath(hh_path))
+    with open(hh_path, 'w') as hh_csv, open(gq_path, 'w') as gq_csv:
+        print('writing', os.path.abspath(hh_path), os.path.abspath(gq_path))
         file_count = 0
         hids = set()
         for in_file_path in in_file_paths:
@@ -150,68 +153,84 @@ def out_hh_file(in_file_paths, hh_path):
                             continue
                         row = line + ',' + more_header
                         aid.write_and_check_columns(hh_csv, row, columns)
+                        aid.write_and_check_columns(gq_csv, row, columns)
                     else:
                         cells = line.split(',')
                         hid = cells[hid_column]
                         if hid not in hids:
                             hids.add(hid)
-                            row = line + ','
+                            row = line + ',' + ',' + str(hid2cnt[hid])
                             aid.write_and_check_columns(hh_csv, row, columns)
 
 
-def save_pp_as_txt_with_reordering_columns(pp_csv):
+def _save_pp_as_txt_with_reordering_columns(pp_csv, gq_pp_csv):
     """ csv columns (input):
-         0 serialno,puma_id,stcotrbg,sp_hh_id,longitude,
+          0 SERIALNO,puma_id,place_id,SYNTHETIC_HID,longitude,
          5 latitude,AGEGRP,HRSWRK,IMMSTAT,INCTAX,
-        10 MODE,OCC,POB,RELIGION,sex,
-        15 sp_id,made-sporder,made-empty,made-sex,made-age
+        10 MODE,OCC,POB,RELIGION,SEX,
+        15 SYNTHETIC_PID,made-sporder,made-empty,made-sex,made-age
         text columns (output):
         sp_id,sp_hh_id,serialno,stcotrbg,age,
         sex,race,sporder,relate,sp_school_id,
         sp_work_id """
-    pp_columns = [16, 4, 1, 3, 20, 19, 18, 17, 18, 18, 18]
+    pp_columns = [16, 4, 1, 3, 20,
+                  19, 18, 17, 18, 18,
+                  18]
     aid.reorder_and_check_header(pp_csv, pp_columns, 'synth_people.txt-ca')
+    # text columns (output):
+    # sp_id,sp_gq_id,sporder,age,sex
+    gq_pp_columns = [16, 4, 17, 20, 19]
+    aid.reorder_and_check_header(gq_pp_csv, gq_pp_columns,
+                                 'synth_gq_people.txt-ca')
 
 
-def save_hh_as_txt_with_reordering_columns(hh_csv):
+def _save_hh_as_txt_with_reordering_columns(hh_csv, gq_csv):
     """ csv columns (input):
-        serialno,puma_id,stcotrbg,sp_id,longitude,
-        latitude,AGEGRP,HRSWRK,IMMSTAT,hh_income,
-        MODE,OCC,POB,RELIGION,SEX,
-        SYNTHETIC_PID,made-empty,sex
+
+         0 SERIALNO,puma_id,place_id,SYNTHETIC_HID,longitude,
+         5 latitude,AGEGRP,HRSWRK,IMMSTAT,INCTAX,
+        10 MODE,OCC,POB,RELIGION,SEX,
+        15 SYNTHETIC_PID,made-empty,made-persons
         text columns (output):
         sp_id,serialno,stcotrbg,hh_race,hh_income,
         hh_size,hh_age,latitude,longitude """
-    hh_columns = [4, 1, 3, 17, 10, 17, 17, 6, 5]
+    hh_columns = [4, 1, 3, 17, 17,
+                  18, 17, 6, 5]
     aid.reorder_and_check_header(hh_csv, hh_columns, 'synth_households.txt-ca')
+    # text columns (output):
+    # sp_id,gq_type,persons,stcotrbg,
+    # latitude,longitude
+    gq_columns = [4, 17, 17, 3,
+                  6, 5]
+    aid.reorder_and_check_header(gq_csv, gq_columns, 'synth_gq.txt-ca')
 
 
-def to_age(agegroup):
-    iterable = age_group2age.get(agegroup, None)
+def _to_age(agegroup):
+    iterable = _age_group2age.get(agegroup, None)
     return next(iterable) if iterable else agegroup
 
 
-def find_csvs_by_iso3_and_prefix(iso3, prefix):
-    result = prefix2csvs.get(prefix, None)
+def _find_csvs_by_iso3_and_prefix(iso3, prefix):
+    result = _prefix2csvs.get(prefix, None)
     if result:
         return result
     return spew.find_csvs_by_iso3_and_prefix(iso3, prefix)
 
 
 def test():
-    prefix2csvs[conf.hh_prefix] = ['spew_sample/can/household_4620440.00.csv']
-    prefix2csvs[conf.pp_prefix] = ['spew_sample/can/people_4620440.00.csv']
+    _prefix2csvs[conf.hh_prefix] = ['spew_sample/can/household_4620440.00.csv']
+    _prefix2csvs[conf.pp_prefix] = ['spew_sample/can/people_4620440.00.csv']
     import filecmp
     translate('can')
     actual = './populations/spew_1.2.0_can'
     expected = './expected/spew_1.2.0_can'
     dcmp = filecmp.dircmp(actual, expected)
-    same_files(dcmp)
+    _same_files(dcmp)
     if dcmp.diff_files:
         raise Exception('Difference: ' + str(dcmp.diff_files))
 
 
-def same_files(dcmp):
+def _same_files(dcmp):
     actual_files = set(dcmp.left_list)
     expected_files = set(dcmp.right_list)
     extras = actual_files.difference(expected_files)
